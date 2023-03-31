f5e1850029612921496ea64bcdc46512c7c06586
Nehaishu24 <nehaom24@gmal.com>
2023-03-31 16:38:58 +0530

    msg

---
+++ ORS_Project4/src/main/java/com/rays/pro4/Exception/DatabaseException.java
@@ -0,0 +1,16 @@
+package com.rays.pro4.Exception;
+
+/**
+ * DatabaseException is propogated by DAO classes when an unhandled Database
+ * exception occurred.
+ * 
+ * @author Neha Sharma
+ */
+
+public class DatabaseException extends Exception {
+
+	public DatabaseException(String msg){
+		super(msg);
+	}
+	
+}
